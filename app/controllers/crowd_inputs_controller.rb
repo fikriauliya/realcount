83a180a21d6185a26bb6ec808eee83d5da6a7836
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-07-12 22:54:03 +0800

    Make counter smarter to detect fake data

--- app/controllers/crowd_inputs_controller.rb
+++ app/controllers/crowd_inputs_controller.rb
@@ -19,9 +19,14 @@ class CrowdInputsController < ApplicationController
 
       @my_progress = CrowdInput.where(:user_id => current_user.id).count()
       @global_progress = CrowdInput.count()
-      @count_prabowo = CrowdInput.sum(:prabowo_count)
-      @count_jokowi = CrowdInput.sum(:jokowi_count)
-      @count_broken = CrowdInput.sum(:broken_count)
+      # @count_prabowo = CrowdInput.sum(:prabowo_count)
+      # @count_jokowi = CrowdInput.sum(:jokowi_count)
+      # @count_broken = CrowdInput.sum(:broken_count)
+
+      ac = CrowdInput.select("tps_id as tps_id, count(tps_id) as amount, min(prabowo_count) as pc, max(prabowo_count), min(jokowi_count) as jc, max(jokowi_count), min(broken_count) as bc, max(broken_count)").group(:tps_id).having("count(tps_id) > 1 and min(prabowo_count) = max(prabowo_count) and min(jokowi_count) = max(jokowi_count) and min(broken_count) = max(broken_count)")
+      @count_prabowo = ac.to_a.sum{|a| a.pc}
+      @count_jokowi = ac.to_a.sum{|a| a.jc}
+      @count_broken = ac.to_a.sum{|a| a.bc}
 
       @top_contributors = CrowdInput.select("user_id as user_id, count(user_id) as amount").group(:user_id).order("amount DESC").limit(10)
     else
