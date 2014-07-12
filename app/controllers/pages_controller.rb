ebc9875fc0168589bbecbfc17e7d40510e7b994d
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-07-12 15:53:26 +0800

    Show statistics in front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to new_crowd_input_path
     end
+    @count_prabowo = CrowdInput.sum(:prabowo_count)
+    @count_jokowi = CrowdInput.sum(:jokowi_count)
+    @count_broken = CrowdInput.sum(:broken_count)
+    @global_progress = CrowdInput.count()
   end
 end
